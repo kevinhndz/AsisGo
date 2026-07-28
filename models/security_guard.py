9eff7c09d3813cc075d5371a3d41d5bbb9ad4459
Kevin <kh925063@gmail.com>
2026-07-28 00:00:10 -0600

    pydantic slight change

--- models/security_guard.py
+++ models/security_guard.py
@@ -12,4 +12,15 @@ class CrearCliente(BaseModel):
     contrasena: str = Field(min_length=8, max_length=15)
 
     
-    
+class CrearClase(BaseModel):
+    nombre: str = Field(min_length=3, max_length=100)
+    codigo: str = Field(min_length=3, max_length=20)
+    horario: str = Field(min_length=5, max_length=100)
+    ubicacion: str = Field(min_length=3, max_length=100)
+
+class RegistrarEstudiante(BaseModel):
+    nombre: str = Field(min_length=2, max_length=100)
+    numero_cuenta: str = Field(min_length=5, max_length=20)
+    email: str
+    modalidad: str  # "Presencial" o "Virtual"
+    id_clase: int
