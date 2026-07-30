e3100c836530be98049e7e48ceef5f1485337b2e
Kevin <kh925063@gmail.com>
2026-07-30 13:41:50 -0600

    Pydantics

--- models/security_guard.py
+++ models/security_guard.py
@@ -3,16 +3,16 @@ from typing import Optional
 
 
 class RevisarDatos(BaseModel):
-    usuario: str = Field(min_length=3, max_length=50)
-    contrasena: str = Field(min_length=8, max_length=255)
+    usuario: str = Field(min_length=6, max_length=10)
+    contrasena: str = Field(min_length=8, max_length=15)
 
 
 class CrearCliente(BaseModel):
     nombre: str = Field(min_length=2, max_length=100)
     telefono: str = Field(min_length=8, max_length=20)
     correo: str = Field(min_length=9, max_length=150)
-    usuario: str = Field(min_length=3, max_length=50)
-    contrasena: str = Field(min_length=8, max_length=255)
+    usuario: str = Field(min_length=6, max_length=10)
+    contrasena: str = Field(min_length=8, max_length=15)
 
 
 class CrearMateria(BaseModel):
@@ -26,12 +26,11 @@ class CrearEstudiante(BaseModel):
     telefono: str = Field(min_length=8, max_length=20)
     correo: str = Field(min_length=9, max_length=150)
     numero_cuenta: str = Field(min_length=4, max_length=20)
-    modalidad: str = Field(min_length=6, max_length=15)  # "presencial" | "virtual"
+    modalidad: str = Field(min_length=6, max_length=15)
 
 
 class MarcarAsistencia(BaseModel):
     token: str = Field(min_length=10, max_length=40)
     numero_cuenta: str = Field(min_length=4, max_length=20)
-
     lat: Optional[float] = None
     lng: Optional[float] = None
