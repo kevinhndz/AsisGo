b71a249b43ed0affa1eea53a03146b8c09179611
Kevin <kh925063@gmail.com>
2026-07-30 13:39:01 -0600

    almacen.py

--- models/almacen.py
+++ models/almacen.py
@@ -3,27 +3,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
 
-# 1. Cargamos el archivo .env para que Python lea las credenciales secretas
 load_dotenv()
 
-# 2. Extraemos cada valor usando os.getenv
 USER = os.getenv("DB_USER")
 PASSWORD = os.getenv("DB_PASSWORD")
 HOST = os.getenv("DB_HOST")
 PORT = os.getenv("DB_PORT")
 NAME = os.getenv("DB_NAME")
 
-#
-DATABASE_URL = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}"
+RUTA_ALMACEN = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}"
 
-
-motor = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=motor)
+motor = create_engine(RUTA_ALMACEN)
+FabricaLLaves = sessionmaker(bind=motor)
 miClaseBase = declarative_base()
 
 
 def abrir_puerta_bd():
-    base_datos = SessionLocal()
+    base_datos = FabricaLLaves()
     try:
         yield base_datos
     finally:
